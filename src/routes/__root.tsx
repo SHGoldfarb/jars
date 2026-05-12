548d49c9959ab3012f1f0d6d7d4c1a4ed1691a15
Samuel Goldfarb <samuelghau@gmail.com>
2026-05-12 01:46:00 -0400

    add tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,4 @@
+import { createRootRoute } from '@tanstack/react-router';
+import { RootLayout } from './-components';
+
+export const Route = createRootRoute({ component: RootLayout });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: false,
+    }),
     react(),
     VitePWA({
       registerType: 'prompt',
