4558d237e4b96a45974e6d69e7a86ba32d1e44f0
Samuel Goldfarb <samuelghau@gmail.com>
2026-05-12 21:42:13 -0400

    fix tslib alias again

--- vite.config.ts
+++ vite.config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      tslib: './node_modules/tslib/tslib.es6.js',
+      tslib: path.resolve(__dirname, './node_modules/tslib/tslib.es6.js'),
     },
   },
 });
