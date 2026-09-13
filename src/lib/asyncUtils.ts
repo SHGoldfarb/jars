4ed9e4d7a6ba48bbc44559ef612f6ce628ae0ee4
Samuel Goldfarb <samuelghau@gmail.com>
2026-09-13 17:56:30 -0300

    add lock

---
+++ src/lib/asyncUtils.ts
@@ -0,0 +1,69 @@
+/**
+ * A wrapper around setTimeout which returns a promise. Useful for waiting for an amount of
+ * time from an async function. e.g. await waitFor(1000);
+ *
+ * @param milliseconds The amount of time to wait.
+ * @returns A promise that resolves once the given number of milliseconds has ellapsed.
+ */
+export const waitFor = (milliseconds: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+};
+
+const emptyPromise = () => {
+  let resolve = () => {
+    // Blank un purpose
+  };
+
+  const promise = new Promise<void>((resolve_) => {
+    resolve = resolve_;
+  });
+
+  return { promise, resolve };
+};
+
+/**
+ * Used by doWithLock() to keep track of each "stack" of locks for a given lock name.
+ */
+const locksByName: Record<string, Promise<unknown>> = {};
+
+/**
+ * Used to ensure that only a single task for the given lock name can be executed at once.
+ * While JS is generally single threaded, this method can be useful when running asynchronous
+ * tasks which may interact with external systems (HTTP API calls, React Native plugins, etc)
+ * which will cause the main JS thread's event loop to become unblocked. By using the same
+ * lock name for a group of tasks you can ensure the only one task will ever be in progress
+ * at a given time.
+ *
+ * @param lockName The name of the lock to be obtained.
+ * @param task The task to execute.
+ * @returns The value returned by the task.
+ */
+export const doWithLock = async <T>(lockName: string, task: () => Promise<T>): Promise<T> => {
+  // Create the lock, which is simply a promise. Obtain the promise's resolve method which
+  // we can use to "unlock" the lock, which signals to the next task in line that it can start.
+  const { resolve: unlock, promise: newLock } = emptyPromise();
+
+  // If there are no previous locks for this lock name, just use a promise that will resolve immediately
+  // as the previous lock.
+  if (!(lockName in locksByName)) {
+    locksByName[lockName] = waitFor(0);
+  }
+
+  // Replace the previous lock with our own in the store.
+  const prevLock = locksByName[lockName];
+  locksByName[lockName] = newLock;
+
+  // Wait for our turn
+  await prevLock;
+
+  // Now that it's our turn, execute the task. We use a finally block here to ensure that we unlock
+  // the lock so the next task can start, even if our task throws an error.
+  try {
+    return await task();
+  } finally {
+    // Invoke unlock to signal to the next waiting task to start.
+    unlock();
+  }
+};
--- src/services/balances/application/queries.ts
+++ src/services/balances/application/queries.ts
@@ -1,5 +1,6 @@
 import { financeQueries } from 'src/services/finance';
 import { cachedBalancesGetters, createBalancesGetters } from '../domain/queries';
+import { doWithLock } from 'src/lib/asyncUtils';
 
 const createBalanceQueries = (financeQueriesDeps: typeof financeQueries) => {
   // The operation ids identify the data the balances are computed from, so they are read first:
@@ -11,21 +12,21 @@ const createBalanceQueries = (financeQueriesDeps: typeof financeQueries) => {
       financeQueriesDeps.allocations.lastOperationId(),
     ].join(':');
 
-  const getBalances = async () => {
-    // TODO: have a lock in balances compute, so that if it's called multiple times at the same time,
-    // it's only computed once and they all hit that cache.
-    const dataStateId = currentDataStateId();
-    // TODO: this should be a cached function that includes the movements fetching inside the function, so that
-    // manually checking for cached value is not necessary.
-    const cached = cachedBalancesGetters(dataStateId);
-    if (cached) {
-      return cached;
-    }
-    const transactions = await financeQueriesDeps.transactions.list();
-    const transfers = await financeQueriesDeps.transfers.list();
-    const allocations = await financeQueriesDeps.allocations.list();
-    return createBalancesGetters({ transactions, transfers, allocations, dataStateId });
-  };
+  const getBalances = () =>
+    // Balances are computed with a lock to prevent multiple concurrent computations.
+    doWithLock('balances', async () => {
+      const dataStateId = currentDataStateId();
+      // TODO: this should be a cached function that includes the movements fetching inside the function, so that
+      // manually checking for cached value is not necessary.
+      const cached = cachedBalancesGetters(dataStateId);
+      if (cached) {
+        return cached;
+      }
+      const transactions = await financeQueriesDeps.transactions.list();
+      const transfers = await financeQueriesDeps.transfers.list();
+      const allocations = await financeQueriesDeps.allocations.list();
+      return createBalancesGetters({ transactions, transfers, allocations, dataStateId });
+    });
 
   return {
     accounts: async (accountId: string) => {
